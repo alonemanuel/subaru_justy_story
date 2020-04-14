a16c1a0f05c9b3267245c7e92d58678cb08e0607
Alon Emanuel <alon@alonemanuel.com>
2020-04-14 20:14:06 +0300

    gif working audio fade working but not turning on always

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-console.log("etnere");
-var container = document.getElementById('container');
-var audioElm = document.getElementById("scrollaudio")[0];
-container.addEventListener('scroll', function () {
-    console.log(audioElm.volume);
-    audioElm.volume = 1 - window.scrollTop() / height;
-    height = window.innerHeight * (window.innerHeight / document.body.offsetHeight);
-});
-var height;
-window.document.addEventListener('click', function () {
-    console.log('here');
-});
-
-
+// console.log("etnere");
+// var container = document.getElementById('container');
+// var flag = true;
+// // container.addEventListener('scroll', function () {
+// //     console.log(audioElm.volume);
+// //     audioElm.volume = 1 - window.scrollTop() / height;
+// //     height = window.innerHeight * (window.innerHeight / document.body.offsetHeight);
+// // });
+// var height;
+// // var audioElm;
+// // window.onload = function () {
+// //     audiElm = document.getElementById("scrollaudio");
+// //     console.log(audioElm.volume);
+// //     // e.innerHTML='Found you';
+// // };
+// // window.document.addEventListener('click', function () {
+// //     var audioElm = window.document.getElementById("scrollaudio")[0];
+// //     console.log(audioElm.volume);
+// //
+// //     audioElm.volume = flag ? 0 : 100;
+// //     flag = !flag;
+// // });
+// //
+//
